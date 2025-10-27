cfd0f7aaef150b36365d8164c686e2f202f801a5
Dainiz Almazan Bustos <85927389+dainizzz@users.noreply.github.com>
2025-10-26 19:01:09 -0700

    10-minute update

--- main.cpp
+++ main.cpp
@@ -3,10 +3,27 @@
 
 #include <iostream>
 #include <chrono>
+#include <list>
+#include <set>
+#include <vector>
+#include <iostream>
 using namespace std;
+//TODO: Come up w/ better variable names
+void readingRace(set<string>&, list<string>&, vector<string>&);
 
 int main() {
-    
+    set<string> set;
+    list<string> list;
+    vector<string> vector;
+
+    // RACE 1: READING
+    readingRace(set, list, vector);
+
+    // RACE 2: SORTING
+
+    // RACE 3: INSERTING
+
+    // RACE 4: DELETING
 
     return 0;
 }
@@ -17,3 +34,6 @@ auto end = high_resolution_clock::now()
 auto duration = duration_cast<milliseconds>(end - start)
 duration.count() references elapsed milliseconds
 */
+void readingRace(set<string> & set, list<string> & list, vector<string> &vector) {
+    ifstream infile("codes.txt");
+}
