bbae7641b6d2ebb18a696f40784034c84dc67944
Dainiz Almazan Bustos <85927389+dainizzz@users.noreply.github.com>
2025-10-26 19:20:57 -0700

    10-minute update

--- main.cpp
+++ main.cpp
@@ -9,7 +9,10 @@
 #include <fstream>
 using namespace std;
 using namespace std::chrono;
-//TODO: Come up w/ better variable names
+
+// readingRace() reads data from a file to a set, list, and vector and outputs how long it took to read data to each.
+// arguments:
+// returns:
 void readingRace(set<string>&, list<string>&, vector<string>&);
 
 int main() {
@@ -35,6 +38,7 @@ auto end = high_resolution_clock::now()
 auto duration = duration_cast<milliseconds>(end - start)
 duration.count() references elapsed milliseconds
 */
+
 void readingRace(set<string> & set, list<string> & list, vector<string> &vector) {
     ifstream infile("codes.txt");
     string temp;
@@ -45,12 +49,34 @@ void readingRace(set<string> & set, list<string> & list, vector<string> &vector)
         set.insert(temp);
     }
     auto end = chrono::high_resolution_clock::now();
-
     auto setDuration = duration_cast<milliseconds>(end - start);
 
+    // Resetting to start of file
+    infile.clear();
+    infile.seekg(0, ios::beg);
+
     // Reading to list
+    start = chrono::high_resolution_clock::now();
+    while (infile >> temp) {
+        list.push_back(temp);
+    }
+    end = chrono::high_resolution_clock::now();
+    auto listDuration = duration_cast<milliseconds>(end - start);
+
+    // Resetting to start of file
+    infile.clear();
+    infile.seekg(0, ios::beg);
 
     // Reading to vector
+    start = chrono::high_resolution_clock::now();
+    while (infile >> temp) {
+        vector.push_back(temp);
+    }
+    end = chrono::high_resolution_clock::now();
+    auto vectorDuration = duration_cast<milliseconds>(end - start);
 
     infile.close();
+
+    // Outputting results
+    cout << "Read \t" << vectorDuration.count() << '\t' << listDuration.count() << '\t' << setDuration.count() << endl;
 }
